26a31236fef2021b12d40734d2fbfe5ec117b0f5
ArpitaDeb <arpitadeb1990@gmail.com>
2020-06-05 04:09:21 +0000

    add mocha to breed test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,19 @@
-const request = require('request');
-const args = process.argv.slice(2);
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log('Request Error:', error);
-  }
-  console.log(response.statusCode);
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('the requested breed is not found');
-  } else
-    console.log(data[0]['description']);
-})
+const fetchBreedDescription = function (breedName, callback) {
+  const request = require('request');
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('Request Error:', error);
+      callback(error, null);
+    }
+    console.log(response.statusCode);
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, 'the requested breed is not found');
+    } else {
+      callback(null, data[0]['description']);
+    }
+  });
+};
+module.exports = { fetchBreedDescription };
